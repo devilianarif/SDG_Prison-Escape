97ba508382492ac452fa632371b181a3c5b7d8f6
Devilian Arif <52937073+devilianarif@users.noreply.github.com>
2025-12-15 13:59:30 +0700

    update script

--- Assets/Aset/Scripts/CardTypeRoll.cs
+++ Assets/Aset/Scripts/CardTypeRoll.cs
@@ -4,19 +4,21 @@ using TMPro;
 
 public class CardTypeRoll : MonoBehaviour
 {
-    public Image card;
-    public string[] cardtypename;
-    public Sprite[] cardTypeSprites;
     public TMP_Text cardTypeText;
     public Button rolltypecard;
     public CardTypeRollHistory history;
     public Button historyButton;
-    public string lastType;
 
     public GameManager gameManager;
+
+    public string lastType;
+
+    // daftar tipe kartu
+    private string[] cardTypes = { "Bad", "Lucky", "Skill" };
+
     void Start()
     {
-        rolltypecard.onClick.AddListener(RandomSprite);
+        rolltypecard.onClick.AddListener(RandomType);
         historyButton.onClick.AddListener(OpenHistory);
 
         historyButton.gameObject.SetActive(true);
@@ -29,24 +31,48 @@ public class CardTypeRoll : MonoBehaviour
             history.OpenPanel();
     }
 
-    void RandomSprite()
+    void RandomType()
     {
-        int index = Random.Range(0, cardTypeSprites.Length);
+        int index = Random.Range(0, cardTypes.Length);
+        string type = cardTypes[index];
 
-        card.sprite = cardTypeSprites[index];
-        string name = cardtypename[index];
-        cardTypeText.text = name;
-        Debug.Log("Kartu terpilih: " + name);
+        cardTypeText.text = type;
+        cardTypeText.color = GetColorByType(type);
+
+        Debug.Log("Kartu terpilih: " + type);
 
         if (history != null)
-            history.AddHistory(name);
+            history.AddHistory(type);
 
-        lastType = name;
+        lastType = type;
 
-        gameManager.playerState.SetTypeCard(lastType); // save realtime
+        gameManager.playerState.SetTypeCard(lastType);
         gameManager.SaveState();
         gameManager.UpdateChecklist();
+    }
 
+    Color GetColorByType(string type)
+    {
+        switch (type)
+        {
+            case "Bad":
+                return HexToColor("AE4E4E");
 
+            case "Lucky":
+                return HexToColor("4AAF6A");
+
+            case "Skill":
+                return HexToColor("4C7BAE");
+
+            default:
+                return Color.white;
+        }
+    }
+
+    Color HexToColor(string hex)
+    {
+        Color color;
+        ColorUtility.TryParseHtmlString("#" + hex, out color);
+        return color;
     }
 }
